c6e7ee5e113be6889094b8e07aab71a7adf91116
Arely Zuleika <96224990+Moonrise777@users.noreply.github.com>
2025-10-04 19:27:55 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost"; // Servidor de la base de datos
-$usuario = "root"; // Usuario predeterminado de XAMPP
-$password = ""; // Contraseña predeterminada (vacía en XAMPP)
-$base_de_datos = "inventariodb"; 
+$host = getenv('MYSQL_HOST') ?: 'db'; // Servidor de la base de datos
+$user = getenv('MYSQL_USER') ?: 'root'; // Usuario predeterminado de XAMPP
+$pass = getenv('MYSQL_PASSWORD') ?: 'root'; // Contraseña predeterminada (vacía en XAMPP)
+$db   = getenv('MYSQL_DATABASE') ?: 'inventariodb';
 
 // Crear conexión
 $conn = new mysqli($host, $usuario, $password, $base_de_datos);
@@ -13,5 +13,6 @@ $conn = new mysqli($host, $usuario, $password, $base_de_datos);
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
-// echo "Conexión exitosa"; // Descomenta para verificar
+ echo "Conexión exitosa"; // Descomenta para verificar
 ?>
+
